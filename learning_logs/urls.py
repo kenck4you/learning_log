9ae77e8e2c9cff46ea6dead4947c5b21736a0d9a
Pfizer <lilkakaka224@gmail.com>
2026-06-23 22:00:11 +0700

    Set up Learning Log with URLs, views, templates, and database models

---
+++ learning_logs/urls.py
@@ -0,0 +1,15 @@
+"""Определяет схемы URL для learning_logs."""
+
+from django.urls import path
+
+from . import views
+
+app_name = 'learning_logs'
+urlpatterns = [
+    # Home page
+    path('', views.index, name='index'),
+    # Page with a list of all topics
+    path('topics/', views.topics, name='topics'),
+    # A page with detailed information on a specific topic
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+]
