ba50f3b5a042836762aa25b180084bc8b4cad439
Pfizer <lilkakaka224@gmail.com>
2026-06-25 17:14:01 +0700

    Add a registration page

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,12 @@
 
 from django.urls import path, include
 
+from . import views
+
 app_name = 'users'
 urlpatterns = [
     # Enable default authorization URL.
-    path('', include('django.contrib.auth.urls')),    
+    path('', include('django.contrib.auth.urls')),
+    # Register page
+    path('register/', views.register, name='register'),  
 ]
